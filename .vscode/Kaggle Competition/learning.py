8003c616a792ca3cf99c595a9b1bb2777d0ec0c6
The Dossier <thedossier.x@gmail.com>
2022-02-17 21:58:15 -0500

    kaggle competition

---
+++ .vscode/Kaggle Competition/learning.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sample_submission = pd.read_csv('/run/media/thedossier/CaseFile102/Kaggle Worlds Competition/data/sample_submission.csv')
+test = pd.read_csv('/run/media/thedossier/CaseFile102/Kaggle Worlds Competition/data/test.csv')
+train = pd.read_csv('/run/media/thedossier/CaseFile102/Kaggle Worlds Competition/data/train.csv')
+test_colors = pd.read_csv('/run/media/thedossier/CaseFile102/Kaggle Worlds Competition/data/colors/test_colors.csv')
+train_colors = pd.read_csv('/run/media/thedossier/CaseFile102/Kaggle Worlds Competition/data/colors/train_colors.csv')
+
+
+
+
+
